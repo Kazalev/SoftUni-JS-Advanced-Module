4a08ce42825939e7ed0c89d787c75eea0de2e39c
Kazalev <kristian.kazalev@abv.bg>
2020-01-07 22:30:07 +0200

    Task

---
+++ Syntax, Functions and Statements/01. String Length.js
@@ -0,0 +1,20 @@
+function solve(x, y, z){
+
+    let sum = x.length + y.length + z.length;
+    let avrg = Math.floor(sum / 3);
+
+    console.log(sum);
+    console.log(avrg);
+}
+
+solve('chocolate', 'ice cream', 'cake');
+
+
+function solve2(...params){
+
+    let sum = params.reduce((a, b) => a + b.length, 0);
+    let avrg = Math.floor(sum / params.length);
+
+    return [sum, avrg];
+}
+console.log(solve2('chocolate', 'ice cream', 'cake').join('\n'));
