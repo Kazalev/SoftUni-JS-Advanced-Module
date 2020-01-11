0e417c90428e0f1eb25944bb85abefe91d2dda2c
Kazalev <kristian.kazalev@abv.bg>
2020-01-12 01:24:53 +0200

    Task

---
+++ Object/Exercise/03. Cappy Juice.js
@@ -0,0 +1,34 @@
+function solve(arr){
+
+    let juices = {};
+    let bottles = {};
+
+    arr.forEach((line) => {
+        let [ name, quantity ] = line.split(' => ');
+        quantity = Number(quantity);
+
+        if(!juices.hasOwnProperty(name)){
+            juices[name] = 0;
+        }
+
+        juices[name] += quantity;
+
+        if(juices[name] >= 1000){
+            bottles[name] = Math.trunc(juices[name] / 1000);
+        }
+    });
+
+    let keys = Object.keys(bottles);
+    for(let name of keys){
+        let quantity = bottles[name];
+        console.log(`${name} => ${quantity}`);
+    }
+}
+
+solve([
+    'Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549'
+    ]);
