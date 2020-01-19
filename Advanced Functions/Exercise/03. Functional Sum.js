820f89abc2f3137b9bdaaae42d17154bbd36f098
Kazalev <kristian.kazalev@abv.bg>
2020-01-19 18:49:03 +0200

    Task

---
+++ Advanced Functions/Exercise/03. Functional Sum.js
@@ -0,0 +1,15 @@
+let add = (function solve(){
+    let sum = 0;
+
+    return function add(num){
+        sum += num;
+
+        add.toString = function() {
+            return sum;
+        }
+
+        return add;
+    }
+}())
+
+console.log(add(1)(6)(-3).toString());
