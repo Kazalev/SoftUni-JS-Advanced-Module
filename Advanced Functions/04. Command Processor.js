2108a1b29ae6e6c396886daf151d3d5df09ec559
Kazalev <kristian.kazalev@abv.bg>
2020-01-19 18:48:14 +0200

    Task

---
+++ Advanced Functions/04. Command Processor.js
@@ -0,0 +1,35 @@
+function solution(){
+
+    let currentText = '';
+    
+    function append(string){
+        currentText += string;
+    }
+
+    function removeStart(n){
+        currentText = currentText.substring(n);
+    }
+
+    function removeEnd(n){
+        currentText = currentText.substring(0, currentText.length - n);
+    }
+
+    function print(){
+        console.log(currentText);
+    }
+
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    }
+}
+
+let firstZeroTest = solution();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
