34c95a8312c680d8526c65c6b1b651e91cf4aaf8
Kazalev <kristian.kazalev@abv.bg>
2020-01-20 21:28:12 +0200

    Task

---
+++ DOM/04. Visited Websites/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+
+  let siteElements = document.getElementsByClassName('link-1');
+    
+  for(let siteElement of siteElements){
+    siteElement.addEventListener('click', (e) => {
+      let currentTarget = e.currentTarget;
+      let paragraphElement = currentTarget.getElementsByTagName('p')[0];
+
+      let text = paragraphElement.textContent;
+      let textParts = text.split(' ');
+      let clicks = Number(textParts[1]);
+      clicks++;
+
+      textParts[1] = clicks;
+      paragraphElement.textContent = textParts.join(' ');
+    });
+  }
+}
