0bfd3327b84ba1518922fcef72daa71bb49ed1d8
Kazalev <kristian.kazalev@abv.bg>
2020-01-22 21:57:13 +0200

    Task

---
+++ DOM Manipulations/03. Accordion/accordion.js
@@ -0,0 +1,13 @@
+function toggle() {
+    
+    const content = document.getElementById('extra');
+    const [ span ] = document.getElementsByTagName('span');
+
+    if(span.textContent === 'More'){
+        span.textContent = 'Less';
+        content.style.display = 'block';
+    } else {
+        span.textContent = 'More';
+        content.style.display = 'none';
+    }
+}
