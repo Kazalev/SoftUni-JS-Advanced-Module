21ce6db16651d5e8d0ebaba9b213f384ccc5a984
Kazalev <kristian.kazalev@abv.bg>
2020-01-23 21:49:07 +0200

    Task

---
+++ DOM Manipulations/05. Notification/scripts.js
@@ -0,0 +1,11 @@
+function notify(message) {
+    
+    const notification = document.getElementById('notification');
+
+    notification.textContent = message;
+    notification.style.display = 'block';
+
+    setTimeout(() => {
+        notification.style.display = 'none';
+    }, 2000);
+}
